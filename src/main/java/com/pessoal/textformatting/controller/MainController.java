1ec61768fa9ffecca2764376f7cc2f797bb1755e
Luiz Henrique Netto <lh.netto11@gmail.com>
2023-11-22 20:46:10 -0300

    Primeiro commit

---
+++ src/main/java/com/pessoal/textformatting/controller/MainController.java
@@ -0,0 +1,14 @@
+package com.pessoal.textformatting.controller;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class HelloController {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to JavaFX Application!");
+    }
+}
---
+++ src/main/java/com/pessoal/textformatting/model/TextFunctions.java
@@ -0,0 +1,2 @@
+package com.pessoal.textformatting.model;public class TextFunctions {
+}
